3a3d79af6f34b4a6792bc2af6e61628209fdb3d1
sofia-papa <papa.sofia@outlook.it>
2021-09-28 17:05:10 +0100

    esercizio completato

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,40 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg=""  />
+    <p v-for="element in emailList" :key="element"> {{ element }}</p>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios';
 
 export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+
+  data: function () {
+    return {
+    emailList: [],
+    email: 10,
+    }
+  },
+
+  mounted() {
+    console.log(axios);
+
+    for (let i= 0; i < this.email; i++) {
+         axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+         .then((response) => {
+              this.emailList.push(response.data.response);
+              console.log(this.emailList);
+    });
+    }
+
+    
   }
 }
 </script>
